1804f97ca815bebaa25f701cad769b6a4ecddf07
Saad T <tariqs26@mcmaster.ca>
2023-02-01 04:52:08 -0500

    reduce redundant if-control, create props type with optional message

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,18 +1,23 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-export default function ProtectedRoute({ element }: { element: JSX.Element }) {
+type Props = {
+  element: JSX.Element;
+  message?: string;
+};
+
+export default function ProtectedRoute({ element, message }: Props) {
   const { user } = useAuth();
 
-  if (user) {
-    return element;
-  } else {
-    return (
-      <Navigate
-        to='/login'
-        replace={true}
-        state={{ from: window.location.pathname }}
-      />
-    );
-  }
+  if (user) return element;
+  return (
+    <Navigate
+      to='/login'
+      replace
+      state={{
+        from: window.location.pathname,
+        message,
+      }}
+    />
+  );
 }
