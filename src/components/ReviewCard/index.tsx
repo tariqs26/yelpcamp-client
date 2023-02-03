de7d3955ddd43245e667b811167e8940eb82e379
Saad T <tariqs26@mcmaster.ca>
2023-02-03 13:24:50 -0500

    review card component

---
+++ src/components/ReviewCard/index.tsx
@@ -0,0 +1,36 @@
+import { Button, Card } from 'react-bootstrap';
+import useDeleteReview from 'hooks/review/useDeleteReview';
+import { Rating } from 'components/Rating';
+
+type Props = {
+  cId: string;
+  review: Review;
+  user: User | undefined;
+};
+
+export default function ReviewCard({ cId, review, user }: Props) {
+  const reviewMutate = useDeleteReview(cId);
+  return (
+    <Card className='mt-3'>
+      <Card.Body>
+        <div className='d-flex justify-content-between'>
+          <Card.Title>{review.author.username}</Card.Title>
+          <Rating rating={review.rating} text={`Rated: ${review.rating}/5`} />
+        </div>
+        <Card.Text>review: {review.body}</Card.Text>
+        {user && user._id === review.author._id && (
+          <Button
+            variant='danger'
+            size='sm'
+            disabled={reviewMutate.isLoading}
+            onClick={() =>
+              reviewMutate.mutate({ id: cId, reviewId: review._id })
+            }
+          >
+            Delete
+          </Button>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
