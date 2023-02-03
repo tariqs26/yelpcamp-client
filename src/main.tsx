4a0c8eb3bd36218cf6f8f89fd05a1794d2d01547
Saad T <tariqs26@mcmaster.ca>
2023-02-02 23:34:58 -0500

    add home page import, and Outlet component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode, lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AlertApi from 'contexts/AlertContext';
 import AuthApi from 'contexts/AuthContext';
+import Home from 'pages/Home';
 
-import Fallback from 'components/Fallback';
-import Footer from 'components/Footer';
 const Register = lazy(() => import('pages/Register'));
 const Login = lazy(() => import('pages/Login'));
 const Campgrounds = lazy(() => import('pages/Campgrounds'));
