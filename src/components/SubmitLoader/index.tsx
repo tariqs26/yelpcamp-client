a4d464e5e0c4aaa21e21a64612fb078588fa6ba7
Saad T <tariqs26@mcmaster.ca>
2023-02-03 13:25:13 -0500

    new loading component for mutations

---
+++ src/components/SubmitLoader/index.tsx
@@ -0,0 +1,24 @@
+import { Spinner } from 'react-bootstrap';
+
+type Props = {
+  text: string;
+  isLoading: boolean;
+};
+
+export default function Loader({ text, isLoading }: Props) {
+  return (
+    <>
+      {isLoading ? (
+        <Spinner
+          as='span'
+          animation='border'
+          size='sm'
+          role='status'
+          aria-hidden='true'
+        />
+      ) : (
+        text
+      )}
+    </>
+  );
+}
