88502e5a526931601ed08432bf3c6d286e425c3c
Saad T <tariqs26@mcmaster.ca>
2023-02-03 12:23:17 -0500

    wrap review form in card for a more pronounced appearnce

--- src/components/ReviewForm/index.tsx
+++ src/components/ReviewForm/index.tsx
@@ -1,32 +1,34 @@
 import useCreateReview from 'hooks/review/useCreateReview';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Card } from 'react-bootstrap';
 import { RatingInput } from 'components/Rating';
 
 export default function ReviewForm({ cId }: { cId: string }) {
   const { handleSubmit, isLoading } = useCreateReview(cId);
   return (
-    <>
-      <h3>Leave a Review</h3>
-      <Form
-        onSubmit={handleSubmit}
-        noValidate
-        onBlur={(e) => {
-          const target = e.currentTarget;
-          target.classList.remove('was-validated');
-        }}
-      >
-        <RatingInput />
-        <Form.Group className='mb-3' controlId='review.body'>
-          <Form.Label>Review:</Form.Label>
-          <Form.Control as='textarea' name='body' rows={3} required />
-          <Form.Control.Feedback type='invalid'>
-            Review cannot be empty
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Button variant='success' type='submit' disabled={isLoading}>
-          Submit
-        </Button>
-      </Form>
-    </>
+    <Card className='mt-3'>
+      <Card.Body>
+        <Card.Title>Leave a Review</Card.Title>
+        <Form
+          onSubmit={handleSubmit}
+          noValidate
+          onBlur={(e) => {
+            const target = e.currentTarget;
+            target.classList.remove('was-validated');
+          }}
+        >
+          <RatingInput />
+          <Form.Group className='mb-3' controlId='review.body'>
+            <Form.Label>Review</Form.Label>
+            <Form.Control as='textarea' name='body' rows={3} required />
+            <Form.Control.Feedback type='invalid'>
+              Review cannot be empty
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant='success' type='submit' disabled={isLoading}>
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 }
