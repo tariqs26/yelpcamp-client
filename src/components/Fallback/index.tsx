d95fe566b29b4d6b87d91df7bcec1dd0c4c5a7be
Saad T <tariqs26@mcmaster.ca>
2023-02-02 23:30:17 -0500

    conditionally render Fallback for Suspense, based on location

--- src/components/Fallback/index.tsx
+++ src/components/Fallback/index.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from 'react-router-dom';
+import LoadingCard from 'components/LoadingCard';
+
 export default function Fallback() {
+  const { pathname } = useLocation();
+  if (pathname.endsWith('campgrounds')) return <LoadingCard />;
   return (
     <div className='d-flex justify-content-center mt-3'>
       <div className='spinner-border text-primary' role='status'>
