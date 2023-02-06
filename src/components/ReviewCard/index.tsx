5c4e302ae55329bad8607c22cb810ece93bc5d08
Saad T <tariqs26@mcmaster.ca>
2023-02-05 22:15:55 -0500

    minor tweak to review card

--- src/components/ReviewCard/index.tsx
+++ src/components/ReviewCard/index.tsx
@@ -14,8 +14,8 @@ export default function ReviewCard({ cId, review, user }: Props) {
   return (
     <Card className='mt-3'>
       <Card.Body>
-        <div className='d-flex justify-content-between'>
-          <Card.Title>{review.author.username}</Card.Title>
+        <div className='d-flex justify-content-between align-items-center'>
+          <Card.Title className='mb-0'>{review.author.username}</Card.Title>
           <Rating rating={review.rating} text={`Rated: ${review.rating}/5`} />
         </div>
         <Card.Text>{review.body}</Card.Text>
