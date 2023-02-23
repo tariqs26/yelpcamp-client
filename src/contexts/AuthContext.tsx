26d970e6d682779ad8dae9cf635247abb660e673
Saad T <tariqs26@mcmaster.ca>
2023-02-23 13:06:35 -0500

    add loading state

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -29,7 +29,15 @@ export default function AuthApi({ children }: { children: React.ReactNode }) {
 
   return (
     <AuthContext.Provider value={memoizedValue}>
-      {!loadingInitial && children}
+      {!loadingInitial ? (
+        children
+      ) : (
+        <div className='d-flex justify-content-center mt-3'>
+          <div className='spinner-border text-primary' role='status'>
+            <span className='visually-hidden'>Loading...</span>
+          </div>
+        </div>
+      )}
     </AuthContext.Provider>
   );
 }
