20e5a04e886f8bc1c68fb11b9bcec37e6662173a
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-08 18:22:02 -0400

    remove initial page animations

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   if (pathname.endsWith("campgrounds"))
     return (
       <footer className="footer bg-dark py-3 px-4 mt-auto">
-        <span className="text-muted">&copy; YelpCamp 2023 </span>
+        <span className="text-muted">&copy; YelpCamp 2023</span>
       </footer>
     )
   return null
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,33 @@
-import { StrictMode, lazy, Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import AlertApi from 'contexts/AlertContext';
-import AuthApi from 'contexts/AuthContext';
-import Home from 'pages/Home';
-import Fallback from 'components/Fallback';
-import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
+import { StrictMode, lazy, Suspense } from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import AlertApi from "contexts/AlertContext"
+import AuthApi from "contexts/AuthContext"
+import Home from "pages/Home"
+import Fallback from "components/Fallback"
+import Navbar from "components/Navbar"
+import Footer from "components/Footer"
 
-const Register = lazy(() => import('pages/Register'));
-const Login = lazy(() => import('pages/Login'));
-const Campgrounds = lazy(() => import('pages/Campgrounds'));
-const Campground = lazy(() => import('pages/Campground'));
-const NewCampground = lazy(() => import('pages/NewCampground'));
-const ProtectedRoute = lazy(() => import('routes/Protected'));
-const NotFound = lazy(() => import('pages/NotFound'));
+const Register = lazy(() => import("pages/Register"))
+const Login = lazy(() => import("pages/Login"))
+const Campgrounds = lazy(() => import("pages/Campgrounds"))
+const Campground = lazy(() => import("pages/Campground"))
+const NewCampground = lazy(() => import("pages/NewCampground"))
+const ProtectedRoute = lazy(() => import("routes/Protected"))
+const NotFound = lazy(() => import("pages/NotFound"))
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 60 * 24,
+      staleTime: 3600 * 24,
     },
   },
-});
+})
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthApi>
@@ -35,32 +35,32 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
           <AlertApi>
             <Navbar />
             <Routes>
-              <Route path='/' element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route
                 element={
-                  <main className='container position-relative my-4'>
+                  <main className="container position-relative my-4">
                     <Suspense fallback={<Fallback />}>
                       <Outlet />
                     </Suspense>
                   </main>
                 }
               >
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/campgrounds'>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/campgrounds">
                   <Route index element={<Campgrounds />} />
-                  <Route path=':id' element={<Campground />} />
+                  <Route path=":id" element={<Campground />} />
                 </Route>
                 <Route
-                  path='new-campground'
+                  path="new-campground"
                   element={
                     <ProtectedRoute
-                      message='Please sign in to create a new campground'
+                      message="Please sign in to create a new campground"
                       element={<NewCampground />}
                     />
                   }
                 />
-                <Route path='*' element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
               </Route>
             </Routes>
             <Footer />
@@ -69,4 +69,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       </AuthApi>
     </QueryClientProvider>
   </StrictMode>
-);
+)
