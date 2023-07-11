e460b561c0d5410f25325d24e68dfed2be299a60
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-11 02:08:09 -0400

    add class merge functionality

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,29 @@
-import day from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import day from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import { type ClassValue, clsx } from "clsx"
 
 const dataFromInput = <T>(form: HTMLFormElement): T =>
-  Object.fromEntries(new FormData(form)) as unknown as T;
+  Object.fromEntries(new FormData(form)) as unknown as T
 
 const handleValidation = (e: React.FormEvent<HTMLFormElement>): boolean => {
-  e.preventDefault();
-  e.stopPropagation();
+  e.preventDefault()
+  e.stopPropagation()
   if (!e.currentTarget.checkValidity()) {
-    e.currentTarget.classList.add('was-validated');
-    return false;
+    e.currentTarget.classList.add("was-validated")
+    return false
   }
-  return true;
-};
+  return true
+}
 
 const isAppError = (data: any): data is AppError => {
-  return 'message' in data && 'details' in data;
-};
+  return "message" in data && "details" in data
+}
 
 const fromDate = (date: string) => {
-  day.extend(relativeTime);
-  return day(date).fromNow();
-};
+  day.extend(relativeTime)
+  return day(date).fromNow()
+}
 
-export { dataFromInput, handleValidation, isAppError, fromDate };
+const cn = (...inputs: ClassValue[]) => clsx(inputs)
+
+export { dataFromInput, handleValidation, isAppError, fromDate, cn }
