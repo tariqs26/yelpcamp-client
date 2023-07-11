65d76635f3eb6d249f6d3fe4d712ec86a6eb6a13
Saad Tariq <tariqs26@mcmaster.ca>
2023-07-11 02:13:13 -0400

    merge type decs to single file

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -17,3 +17,53 @@ type MutationError = Error & {
     data: string
   }
 }
+
+type Campground = {
+  _id: string
+  author: AppUser
+  title: string
+  image: string
+  location: string
+  price: number
+  reviews: Review[]
+  description: string
+  createdAt: string
+  updatedAt: string
+  geometry: {
+    type: "Point"
+    coordinates: [number, number]
+  }
+}
+
+type CampgroundInput = Pick<
+  Campground,
+  "title" | "image" | "location" | "price" | "description"
+>
+
+type CampgroundsData =
+  | {
+      pageParams?: unknown[]
+      pages?: { campgrounds: Pick<Campground, "_id">[]; totalPages: number }[]
+    }
+  | undefined
+
+type Review = {
+  _id: string
+  author: AppUser
+  rating: number
+  body: string
+}
+
+type ReviewInput = Omit<Review, "_id" | "author">
+
+type User = {
+  _id: string
+  username: string
+  email: string
+  password: string
+}
+
+type UserInput = Omit<User, "_id">
+type AppUser = Omit<User, "password"> & {
+  isAdmin: boolean
+}
--- src/types/review.d.ts
+++
@@ -1,8 +0,0 @@
-type Review = {
-  _id: string
-  author: AppUser
-  rating: number
-  body: string
-}
-
-type ReviewInput = Omit<Review, "_id" | "author">
