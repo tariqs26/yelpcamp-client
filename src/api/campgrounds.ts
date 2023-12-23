19d5ce73d7333e8e81badd80afe37196cf970d2b
Saad Tariq <saadtariq.dev@outlook.com>
2023-12-23 17:55:18 -0500

    fix: remove timeout

--- src/api/campgrounds.ts
+++ src/api/campgrounds.ts
@@ -10,7 +10,6 @@ export const fetchCampgrounds = async ({
   campgrounds: Campground[]
   totalPages: number
 }> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
   return (await campgroundsAPI.get("/", { params: { page } })).data
 }
 
