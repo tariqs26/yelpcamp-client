bc05faa4a1a57a5994f28254676b830fd72ce821
Saad Tariq <saadtariq.dev@outlook.com>
2023-12-23 17:55:42 -0500

    refactor: custom timeout error message

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,6 +5,7 @@ const axiosInstance = (extendUrl: string = "") => {
     baseURL: `${import.meta.env.VITE_API_URL}${extendUrl}`,
     withCredentials: true,
     timeout: 5000, // 5 seconds
+    timeoutErrorMessage: "The server took too long to respond.",
   })
   return instance
 }
