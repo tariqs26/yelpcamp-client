7415db2558dd2fa20549985fa1233788a45d4b33
Saad Tariq <saadtariq.dev@outlook.com>
2024-02-23 10:05:32 -0500

    refactor: convert to default export

--- src/types/errors.ts
+++ src/types/errors.ts
@@ -1,4 +1,4 @@
-export const ErrorDetails = {
+const ErrorDetails = {
   SERVER_ERROR:
     "Something went wrong, the server is currently unavailable, please try again later",
   NOT_FOUND: "The campground you are looking for does not exist",
@@ -6,3 +6,5 @@ export const ErrorDetails = {
   FAILED_REGISTER: "Network error: Failed to register user",
   FAILED_LOGIN: "Network error: Failed to login user",
 } as const
+
+export default ErrorDetails
