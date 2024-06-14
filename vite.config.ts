185915f79a28e0eb6f13e5b90daf2dd21b5ad25d
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-14 03:04:24 -0400

    chore: remove eslint plugin from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
-import eslint from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    eslint({
-      failOnError: false,
-    }),
   ],
   server: {
     port: 3001,
