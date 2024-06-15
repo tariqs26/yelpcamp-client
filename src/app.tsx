93142fc68b43db8c9930f0f3621837b3c473eaa8
Saad Tariq <saadtariq.dev@outlook.com>
2024-06-15 02:59:12 -0400

    refactor: extract main to separate component

---
+++ src/app.tsx
@@ -0,0 +1,46 @@
+import { lazy } from "react"
+import { Route, Routes } from "react-router-dom"
+
+import Footer from "components/footer"
+import AuthLayout from "components/layouts/auth-layout"
+import NavLayout from "components/layouts/nav-layout"
+import SuspenseLayout from "components/layouts/suspense-layout"
+import ProtectedRoute from "components/protected"
+import Login from "pages/auth/login"
+import Register from "pages/auth/register"
+import Home from "pages/home"
+import NotFound from "pages/not-found"
+
+const [Campgrounds, Campground, NewCampground] = [
+  lazy(async () => await import("pages/campground/main")),
+  lazy(async () => await import("pages/campground/view")),
+  lazy(async () => await import("pages/campground/create")),
+]
+
+export default function App() {
+  return (
+    <main className="d-flex flex-column min-vh-100">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route element={<NavLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route element={<SuspenseLayout />}>
+            <Route path="/campgrounds">
+              <Route index element={<Campgrounds />} />
+              <Route path=":id" element={<Campground />} />
+            </Route>
+            <Route
+              path="new-campground"
+              element={<ProtectedRoute element={<NewCampground />} />}
+            />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </main>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,11 @@
-import { StrictMode, lazy } from "react"
+import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
+import { BrowserRouter } from "react-router-dom"
 
-import ReactQueryProvider from "components/providers/react-query"
+import App from "app"
 import AuthProvider from "components/providers/auth"
-import Home from "pages/home"
-import Register from "pages/auth/register"
-import Login from "pages/auth/login"
-import Footer from "components/footer"
-import NavLayout from "components/layouts/nav-layout"
-import SuspenseLayout from "components/layouts/suspense-layout"
-import AuthLayout from "components/layouts/auth-layout"
-import ProtectedRoute from "components/protected"
-import NotFound from "pages/not-found"
-
-const [Campgrounds, Campground, NewCampground] = [
-  lazy(async () => await import("pages/campground/main")),
-  lazy(async () => await import("pages/campground/view")),
-  lazy(async () => await import("pages/campground/create")),
-]
+import ReactQueryProvider from "components/providers/react-query"
 
 // biome-ignore lint/style/noNonNullAssertion: root is always present
 const root = ReactDOM.createRoot(document.getElementById("root")!)
@@ -30,29 +16,7 @@ root.render(
     <ReactQueryProvider>
       <AuthProvider>
         <BrowserRouter>
-          <main className="d-flex flex-column min-vh-100">
-            <Routes>
-              <Route element={<AuthLayout />}>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-              </Route>
-              <Route element={<NavLayout />}>
-                <Route path="/" element={<Home />} />
-                <Route element={<SuspenseLayout />}>
-                  <Route path="/campgrounds">
-                    <Route index element={<Campgrounds />} />
-                    <Route path=":id" element={<Campground />} />
-                  </Route>
-                  <Route
-                    path="new-campground"
-                    element={<ProtectedRoute element={<NewCampground />} />}
-                  />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-            <Footer />
-          </main>
+          <App />
         </BrowserRouter>
       </AuthProvider>
     </ReactQueryProvider>
