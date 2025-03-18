46124f9da459e96374c40917c15709c30f4f46c0
Saad Tariq <saadtariq.dev@outlook.com>
2025-03-18 01:39:06 -0400

    refactor: configure proxy for API requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = (extendUrl = "") => {
   const instance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}${extendUrl}`,
+    baseURL: `/api${extendUrl}`,
     withCredentials: true,
     timeout: 5000, // 5 seconds
     timeoutErrorMessage: "The server took too long to respond.",
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "VITE_")
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ""),
+      },
+    },
   },
 })
