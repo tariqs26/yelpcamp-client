845ad7baab72008f86ac432f1fd89f6b15536cbe
Saad Tariq <saadtariq.dev@outlook.com>
2025-03-18 03:25:00 -0400

    revert: use environment variable for API base URL instead of Vite proxy

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = (extendUrl = "") => {
   const instance = axios.create({
-    baseURL: `/api${extendUrl}`,
+    baseURL: `${import.meta.env.VITE_API_URL}${extendUrl}`,
     withCredentials: true,
     timeout: 5000, // 5 seconds
     timeoutErrorMessage: "The server took too long to respond.",
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-
-const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "VITE_")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
-    proxy: {
-      "/api": {
-        target: env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  server: { port: 3001 },
 })
