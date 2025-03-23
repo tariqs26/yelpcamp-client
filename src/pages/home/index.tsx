4575b19cc06b4ff3e3c16a5d30232b19f258dfbc
Saad Tariq <saadtariq.dev@outlook.com>
2025-03-22 20:28:43 -0400

    refactor: rename Home component and image import and improve card structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom"
 import { Card } from "react-bootstrap"
-import bgImage from "~/assets/images/home-image.webp"
+import homeImage from "~/assets/images/home-image.webp"
 import "./index.css"
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div
       className="cover-container d-flex flex-grow-1 flex-column w-100 p-sm-3 bg-dark text-white justify-content-center align-items-center"
       style={{
-        backgroundImage: `url(${bgImage})`,
+        backgroundImage: `url(${homeImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -33,27 +33,33 @@ export default function Home() {
           </p>
         </div>
         <div className="cards">
-          <div className="card bg-dark p-3">
-            <Card.Title>View campgrounds</Card.Title>
-            <Card.Text>
-              Browse through our hand-picked campgrounds from all over the
-              world.
-            </Card.Text>
-          </div>
-          <div className="card bg-dark p-3">
-            <Card.Title>Share your campground</Card.Title>
-            <Card.Text>
-              Share your campground with the world and help others find their
-              next adventure.
-            </Card.Text>
-          </div>
-          <div className="card bg-dark p-3">
-            <Card.Title>Leave a review</Card.Title>
-            <Card.Text>
-              Leave a review for campgrounds you have visited and help others
-              find the best places to camp.
-            </Card.Text>
-          </div>
+          <Card bg="dark" text="white" about="View campgrounds">
+            <Card.Body>
+              <Card.Title>View campgrounds</Card.Title>
+              <Card.Text>
+                Browse through our hand-picked campgrounds from all over the
+                world.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card bg="dark" text="white">
+            <Card.Body>
+              <Card.Title>Share your campground</Card.Title>
+              <Card.Text>
+                Share your campground with the world and help others find their
+                next adventure.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card bg="dark" text="white">
+            <Card.Body>
+              <Card.Title>Leave a review</Card.Title>
+              <Card.Text>
+                Leave a review for campgrounds you have visited and help others
+                find the best places to camp.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
       </main>
     </div>
